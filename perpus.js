eb398fcd66d3734d9129cae525d45b499f604f48
ersaputriferaya <100006721+ersaputriferaya@users.noreply.github.com>
2023-01-31 08:40:57 +0700

    Update perpus.js

--- perpus.js
+++ perpus.js
@@ -2,7 +2,6 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const mysql = require("mysql")
-const e = require("express")
 
 const app = express()
 app.use(cors())
